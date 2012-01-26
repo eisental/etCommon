6888c4007e1c9eca0715fb6e9f06bd0b1d615de9
Tal Eisenberg <eisental@gmail.com>
2012-01-27 00:11:09 +0100

    First commit

---
+++ src/main/java/net/eisental/common/etCommon.java
@@ -0,0 +1,19 @@
+package net.eisental.common;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Tal Eisenberg
+ */
+public class etCommon extends JavaPlugin {
+
+    @Override
+    public void onDisable() {}
+
+    @Override
+    public void onEnable() {}
+    
+    @Override
+    public void onLoad() {}
+}
